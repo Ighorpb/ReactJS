e6f2533b940eec6c520d117011da507a1560eed8
unknown <pb.ighor@gmail.com>
2023-04-11 15:22:51 -0300

    Criando o caminho para a API do GitHub

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,13 +1,17 @@
-export function RepositoryItem() {
+// Criação do Repository List que vai ser enviado para o List que vai ser enviado para o App
+
+export function RepositoryItem(props) {
         return (
             <li>
-            <strong>unform</strong>
+            <strong>{props.repository.name}</strong>
             <p>Forms in ReactJS</p>
 
-            <a href="">
+            <a href={props.repository.html_url}>
                 Acessar repositórios
             </a>
         </li>    
    )
         
 }
+
+// Componentes do ReactJS tem a mesma funcionalidade de uma tag HTML. Sempre começar o componente com letra maiscúla 'A'
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,17 +1,30 @@
+// Vai receber o conteúdo de RepositoryItem
+
+import { useState, useEffect } from "react"; // useeffect -> Disparar uma função quando algo acontencer na minha aplicação.
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+
+// https://api.github.com/orgs/rocketseet/repos
 
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/ighorxD/repos')
+          .then(response => response.json())
+          .then(data => setRepositories(data));
+    }, [] );
+
+
     return (
        <section className="repository-list">
         <h1>Lista de Repositórios</h1>
 
         <ul>
-            <RepositoryItem />
-            <RepositoryItem />
-            <RepositoryItem />
-            <RepositoryItem />
-
+            {repositories.map(repository => {
+                return <RepositoryItem key ={repository.name}repository = {repository}/>
+             })}
         </ul>
        </section> 
     );
